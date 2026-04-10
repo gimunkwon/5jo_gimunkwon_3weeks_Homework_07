21b65ffdae0db12beb3d8148c7e8d7be9cfa7ce5
gimunkwon <gimon71@gmail.com>
2026-04-10 23:15:33 +0900

    [add] GamemodeClass 생성자 로직
    PlayerControllerClass 값 초기화

--- Source/NBC_HomeWork_07/Private/GlobalSetiings/NBC_Gamemode.cpp
+++ Source/NBC_HomeWork_07/Private/GlobalSetiings/NBC_Gamemode.cpp
@@ -1,8 +1,10 @@
 #include "GlobalSetiings/NBC_Gamemode.h"
 
 #include "PawnObject/MovingPawn.h"
+#include "PawnObject/Controller/BasePlayerController.h"
 
 ANBC_Gamemode::ANBC_Gamemode()
 {
 	DefaultPawnClass = AMovingPawn::StaticClass();
+	PlayerControllerClass = ABasePlayerController::StaticClass();
 }
