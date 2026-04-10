8487977e7ea9548eda73dd597d307d22ce1bed30
gimunkwon <gimon71@gmail.com>
2026-04-10 17:17:47 +0900

    [add] PlayerController에 IMC 매핑 로직 추가

--- Source/NBC_HomeWork_07/Private/PawnObject/Controller/BasePlayerController.cpp
+++ Source/NBC_HomeWork_07/Private/PawnObject/Controller/BasePlayerController.cpp
@@ -1,1 +1,21 @@
 #include "PawnObject/Controller/BasePlayerController.h"
+#include "EnhancedInputSubsystems.h"
+
+
+ABasePlayerController::ABasePlayerController()
+{
+	IMC_Default = nullptr;
+	IA_Look = nullptr;
+	IA_Move = nullptr;
+}
+
+void ABasePlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	if (UEnhancedInputLocalPlayerSubsystem* SubSystem = 
+		Cast<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()->GetSubsystem<UEnhancedInputLocalPlayerSubsystem>()))
+	{
+		SubSystem->AddMappingContext(IMC_Default, 0);
+	}
+	
+}
--- Source/NBC_HomeWork_07/Public/PawnObject/Controller/BasePlayerController.h
+++ Source/NBC_HomeWork_07/Public/PawnObject/Controller/BasePlayerController.h
@@ -4,8 +4,23 @@
 #include "GameFramework/PlayerController.h"
 #include "BasePlayerController.generated.h"
 
+class UInputAction;
+class UInputMappingContext;
+
 UCLASS()
 class NBC_HOMEWORK_07_API ABasePlayerController : public APlayerController
 {
 	GENERATED_BODY()
+public:
+	ABasePlayerController();
+	
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category="Input|InputMappingContext")
+	TObjectPtr<UInputMappingContext> IMC_Default;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category="Input|InputAction")
+	TObjectPtr<UInputAction> IA_Move;
+	UPROPERTY(EditAnywhere,BlueprintReadWrite, Category="Input|InputAction")
+	TObjectPtr<UInputAction> IA_Look;
+	
+protected:
+	virtual void BeginPlay() override;
 };
