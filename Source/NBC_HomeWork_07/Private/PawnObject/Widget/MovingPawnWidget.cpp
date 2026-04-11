ac059cafecd2d5a57f1950f8e341300f2213147e
gimunkwon <gimon71@gmail.com>
2026-04-11 22:04:28 +0900

    [add] MovingPawnWidget 델리게이트 바인딩및, 바인딩 함수추가

--- Source/NBC_HomeWork_07/Private/PawnObject/Widget/MovingPawnWidget.cpp
+++ Source/NBC_HomeWork_07/Private/PawnObject/Widget/MovingPawnWidget.cpp
@@ -1,1 +1,46 @@
 #include "PawnObject/Widget/MovingPawnWidget.h"
+
+#include "Components/Image.h"
+#include "Components/RadialSlider.h"
+#include "Components/TextBlock.h"
+#include "PawnObject/MovingPawn.h"
+
+void UMovingPawnWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+	
+	if (AMovingPawn* OwnerPawn = Cast<AMovingPawn>(GetOwningPlayerPawn()))
+	{
+		OwnerPawn->OnAirModeChanged.AddDynamic(this, &UMovingPawnWidget::UpdateAirModeUI);
+		OwnerPawn->OnSpeedChanged.AddDynamic(this, &UMovingPawnWidget::OnSpeedChanged);
+		OwnerPawn->OnSpeedValueChanged.AddDynamic(this, &UMovingPawnWidget::OnSpeedValueChanged);
+	}
+}
+
+void UMovingPawnWidget::UpdateAirModeUI(bool bIsAirMode)
+{
+	if (Image_AirMode)
+	{
+		float AlphaValue = bIsAirMode ? 0.1f : 1.f;
+		Image_AirMode->SetRenderOpacity(AlphaValue);
+	}
+}
+
+void UMovingPawnWidget::OnSpeedChanged(float ChangeSpeedAmount)
+{
+	if (Slider_Tacometer)
+	{
+		Slider_Tacometer->SetValue(ChangeSpeedAmount);
+	}
+}
+
+void UMovingPawnWidget::OnSpeedValueChanged(float ChangedSpeedValue, float MaxSpeedValue)
+{
+	if (Text_CurrentSpeed)
+	{
+		FString SpeedValue = FString::Printf(TEXT("%.1f/%.1f"),ChangedSpeedValue,MaxSpeedValue);
+		Text_CurrentSpeed->SetText(FText::FromString(SpeedValue));
+	}
+}
+
+
--- Source/NBC_HomeWork_07/Public/PawnObject/Widget/MovingPawnWidget.h
+++ Source/NBC_HomeWork_07/Public/PawnObject/Widget/MovingPawnWidget.h
@@ -4,8 +4,31 @@
 #include "Blueprint/UserWidget.h"
 #include "MovingPawnWidget.generated.h"
 
+class UTextBlock;
+class URadialSlider;
+class UImage;
+
 UCLASS()
 class NBC_HOMEWORK_07_API UMovingPawnWidget : public UUserWidget
 {
 	GENERATED_BODY()
+	
+public:
+	virtual void NativeConstruct() override;
+	
+protected:
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UImage> Image_AirMode;
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<URadialSlider> Slider_Tacometer;
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UTextBlock> Text_CurrentSpeed;
+	
+	
+	UFUNCTION()
+	void UpdateAirModeUI(bool bIsAirMode);
+	UFUNCTION()
+	void OnSpeedChanged(float ChangeSpeedAmount);
+	UFUNCTION()
+	void OnSpeedValueChanged(float ChangedSpeedValue, float MaxSpeedValue);
 };
