600f92229c2311d09d7290fc6edd474808f770cf
gimunkwon <gimon71@gmail.com>
2026-04-11 20:29:49 +0900

    [add] MovingPawnWidget 클래스 추가

--- Source/NBC_HomeWork_07/Public/PawnObject/Widget/MovingPawnWidget.h
+++ Source/NBC_HomeWork_07/Public/PawnObject/Widget/MovingPawnWidget.h
@@ -1,14 +1,9 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
 #include "MovingPawnWidget.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class NBC_HOMEWORK_07_API UMovingPawnWidget : public UUserWidget
 {
