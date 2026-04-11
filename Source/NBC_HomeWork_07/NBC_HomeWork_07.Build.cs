ff6a6d9f47c33124f99c85802535f18648cc7a63
gimunkwon <gimon71@gmail.com>
2026-04-11 20:30:51 +0900

    [add] 위젯사용하기위해 UMG,SlateCore,Slate 모듈 추가

--- Source/NBC_HomeWork_07/NBC_HomeWork_07.Build.cs
+++ Source/NBC_HomeWork_07/NBC_HomeWork_07.Build.cs
@@ -8,7 +8,11 @@ public class NBC_HomeWork_07 : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput"
+			,"UMG","SlateCore","Slate"
+		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
